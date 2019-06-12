6e33833a57799bc0f49703b6a338882207b160da
Emre Keskin <emre.keskin@oplog.com.tr>
2019-06-12 20:17:05 +0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Navbar,
+  ButtonToolbar,
+  Button,
+  Card,
+  Row,
+  Col,
+  ListGroup,
+  Container,
+  Image
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          E-Commerce Cart App
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container fluid>
+        <Row>
+          <Col>
+            <h4>Sizes:</h4>
+            <ButtonToolbar>
+              <Button variant="outline-dark" className="">
+                XS
+              </Button>
+              <Button variant="outline-dark">S</Button>
+              <Button variant="outline-dark">M</Button>
+              <Button variant="outline-dark">L</Button>
+              <Button variant="outline-dark">XL</Button>
+            </ButtonToolbar>
+          </Col>
+          <Col md="auto">
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            
+          </Col>
+
+          <Col>
+            <Card>
+              <Card.Header>Cart</Card.Header>
+              <Card.Body>
+                <Card.Title>Products</Card.Title>
+
+                <Row>
+                  <Col>
+                    <Image src="https://via.placeholder.com/50" />
+                  </Col>
+                  <Col xs={7}>
+                    With supporting text below as a natural lead-in
+                  </Col>
+                  <Col className="text-right">10 $</Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer className="text-muted">
+                <Row>
+                  <Col>
+                    <Button variant="primary">Checkout</Button>
+                  </Col>
+                  <Col xs={7}>
+                    <h3 className="float-right"> 25 $</h3>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
