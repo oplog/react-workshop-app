58f302919b57625702b02d329b4282225758f59c
Uğur Oruç <uoruc5@hotmail.com>
2019-06-13 17:15:40 +0300

    app basic

---
+++ src/products.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    title: "New Balance",
+    text: "Erkek Sweatshirt",
+    price: 80,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/2478813/1/201731010002_1_org.jpg",
+    size: "XL"
+  },
+
+  {
+    title: "Defacto",
+    text: "Kadin Elbise",
+    price: 35,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/60/3554731/1/8681816654247_1_org.jpg",
+    size: "M"
+  },
+  {
+    title: "New Balance",
+    text: "Erkek Sweatshirt",
+    price: 89,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/2478811/1/201801020065_1_org.jpg",
+    size: "L"
+  },
+  {
+    title: "New Balance",
+    text: "Erkek Sweatshirt",
+    price: 80,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/2478813/1/201731010002_1_org.jpg",
+    size: "L"
+  },
+  {
+    title: "Defacto",
+    text: "Kadin Pantolon",
+    price: 50,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/3108255/1/8681555577739_1_org.jpg",
+    size: "S"
+  },
+  {
+    title: "New Balance",
+    text: "Erkek Sweatshirt",
+    price: 89,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/2478811/1/201801020065_1_org.jpg",
+    size: "XL"
+  },
+  {
+    title: "Defacto",
+    text: "Kadin Pantolon",
+    price: 89,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/47/3108255/1/8681555577739_1_org.jpg",
+    size: "M"
+  },
+  {
+    title: "Defacto",
+    text: "Kadin Elbise",
+    price: 89,
+    image: "https://img-trendyol.mncdn.com/Assets/ProductImages/oa/60/3554731/1/8681816654247_1_org.jpg",
+    size: "S"
+  }
+]
