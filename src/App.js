5b440ed0165d3e2572a3d838890f8b12d4e9bf72
Uğur Oruç <uoruc5@hotmail.com>
2019-06-13 16:40:31 +0300

    Revert "app"
    This reverts commit 34c96d6b62ad7ffc9095b0541fea294cd8cfc556.

--- src/App.js
+++ src/App.js
@@ -1,74 +1,137 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/navbar";
-import ShoppingCart from "./components/shoppingCart";
-import Products from "./products";
 import {
+  Navbar,
   ButtonToolbar,
   Button,
+  Card,
   Row,
   Col,
+  ListGroup,
   Container,
+  Image
 } from "react-bootstrap";
-import ShoppingItem from "./components/shoppingItem";
 
-const sizes = ["XS", "S", "M", "L", "XL"];
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedSize: null,
-      cart: []
-    }
-  }
-  changeSize(size) {
-    let selectedSize = this.state.selectedSize === size ? "" : size;
-    this.setState({
-      selectedSize
-    })
-  }
-  addCart(product) {
-    let { cart } = this.state;
-    this.setState({
-      cart: [...cart, product]
-    })
-  }
-  render() {
-    return (
-      <>
-        <Header />
-        <Container fluid>
-          <Row>
-            <Col sm={2}>
-              <h4>Sizes:</h4>
-              <ButtonToolbar>
-                {sizes.map(size => {
-                  return (
-                    <Button variant={this.state.selectedSize === size ? "primary" : "outline-dark"} onClick={() => this.changeSize(size)}>{size}</Button>
-                  )
-                })}
-              </ButtonToolbar>
-            </Col>
-            <Col sm={6}>
-              <Row>
-                {Products.map(product => {
-                  if (!this.state.selectedSize || this.state.selectedSize === product.size) {
-                    return (
-                      <ShoppingItem {...product} onClick={() => this.addCart(product)} />
-                    )
-                  };
-                  return null;
-                })}
-              </Row>
-            </Col>
-            <Col sm={4}>
-              <ShoppingCart cart={this.state.cart} />
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          E-Commerce Cart App
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container fluid>
+        <Row>
+          <Col>
+            <h4>Sizes:</h4>
+            <ButtonToolbar>
+              <Button variant="outline-dark" className="">
+                XS
+              </Button>
+              <Button variant="outline-dark">S</Button>
+              <Button variant="outline-dark">M</Button>
+              <Button variant="outline-dark">L</Button>
+              <Button variant="outline-dark">XL</Button>
+            </ButtonToolbar>
+          </Col>
+          <Col md="auto">
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            
+          </Col>
+
+          <Col>
+            <Card>
+              <Card.Header>Cart</Card.Header>
+              <Card.Body>
+                <Card.Title>Products</Card.Title>
+
+                <Row>
+                  <Col>
+                    <Image src="https://via.placeholder.com/50" />
+                  </Col>
+                  <Col xs={7}>
+                    With supporting text below as a natural lead-in
+                  </Col>
+                  <Col className="text-right">10 $</Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer className="text-muted">
+                <Row>
+                  <Col>
+                    <Button variant="primary">Checkout</Button>
+                  </Col>
+                  <Col xs={7}>
+                    <h3 className="float-right"> 25 $</h3>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
